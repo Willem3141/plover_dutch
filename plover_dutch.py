b3d10cdde44dca54db2a786cab9f8fce741216c3
Willem Sonke <Willem3141@users.noreply.github.com>
2018-10-08 23:54:50 +0200

    Add initial dictionary and system

---
+++ plover_dutch.py
@@ -0,0 +1,95 @@
+KEYS = (
+    '#',
+    'S-', 'T-', 'K-', 'P-', 'W-', 'H-', 'R-',
+    'A-', 'O-',
+    '*',
+    '-E', '-U',
+    '-F', '-R', '-P', '-B', '-L', '-G', '-T', '-S', '-D', '-Z',
+)
+
+IMPLICIT_HYPHEN_KEYS = ('A-', 'O-', '5-', '0-', '-E', '-U', '*')
+
+SUFFIX_KEYS = ('-S', '-G', '-Z', '-D')
+
+NUMBER_KEY = '#'
+
+NUMBERS = {
+    'S-': '1-',
+    'T-': '2-',
+    'P-': '3-',
+    'H-': '4-',
+    'A-': '5-',
+    'O-': '0-',
+    '-F': '-6',
+    '-P': '-7',
+    '-L': '-8',
+    '-T': '-9',
+}
+
+UNDO_STROKE_STENO = '*'
+
+ORTHOGRAPHY_RULES = [
+]
+
+ORTHOGRAPHY_RULES_ALIASES = {}
+
+ORTHOGRAPHY_WORDLIST = None
+
+KEYMAPS = {
+    'Keyboard': {
+        '#'         : ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='),
+        'S-'        : ('a', 'q'),
+        'T-'        : 'w',
+        'K-'        : 's',
+        'P-'        : 'e',
+        'W-'        : 'd',
+        'H-'        : 'r',
+        'R-'        : 'f',
+        'A-'        : 'c',
+        'O-'        : 'v',
+        '*'         : ('t', 'g', 'y', 'h'),
+        '-E'        : 'n',
+        '-U'        : 'm',
+        '-F'        : 'u',
+        '-R'        : 'j',
+        '-P'        : 'i',
+        '-B'        : 'k',
+        '-L'        : 'o',
+        '-G'        : 'l',
+        '-T'        : 'p',
+        '-S'        : ';',
+        '-D'        : '[',
+        '-Z'        : '\'',
+        'arpeggiate': 'space',
+        # Suppress adjacent keys to prevent miss-strokes.
+        'no-op'     : ('z', 'x', 'b', ',', '.', '/', ']', '\\'),
+    },
+    'TX Bolt': {
+        '#'    : '#',
+        'S-'   : 'S-',
+        'T-'   : 'T-',
+        'K-'   : 'K-',
+        'P-'   : 'P-',
+        'W-'   : 'W-',
+        'H-'   : 'H-',
+        'R-'   : 'R-',
+        'A-'   : 'A-',
+        'O-'   : 'O-',
+        '*'    : '*',
+        '-E'   : '-E',
+        '-U'   : '-U',
+        '-F'   : '-F',
+        '-R'   : '-R',
+        '-P'   : '-P',
+        '-B'   : '-B',
+        '-L'   : '-L',
+        '-G'   : '-G',
+        '-T'   : '-T',
+        '-S'   : '-S',
+        '-D'   : '-D',
+        '-Z'   : '-Z',
+    },
+}
+
+DICTIONARIES_ROOT = 'asset:plover:assets'
+DEFAULT_DICTIONARIES = ()
---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+__requires__ = '''
+setuptools>=34.4.0
+'''
+
+from setuptools import setup
+
+setup()
